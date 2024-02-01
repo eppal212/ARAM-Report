de5e51d70cf419a7259162855212aea909db6f1a
유환준 <eppal1205@platb.ai>
2024-02-01 15:33:54 +0900

    [세팅]  프로젝트 생성

---
+++ KalbaramTier/KalbaramTierApp.swift
@@ -0,0 +1,17 @@
+//
+//  KalbaramTierApp.swift
+//  KalbaramTier
+//
+//  Created by OBeris on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct KalbaramTierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
