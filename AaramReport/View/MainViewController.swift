c058a37131bfb91c6baa38c015c09928ebf59597
유환준 <>
2024-02-04 20:28:35 +0900

    [기능] 간단한 프로젝트 구조 정리

--- AaramReport/View/MainViewController.swift
+++ AaramReport/View/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  AaramReport
 //
 //  Created by OBeris on 2/3/24.
@@ -7,8 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
