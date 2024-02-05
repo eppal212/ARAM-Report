85ad7e5996ea692c5a9cf07526f940b4a899aa1b
유환준 <eppal212@naver.com>
2024-02-05 23:38:24 +0900

    [기능] MaskingLabel 컴포넌트 추가

---
+++ AaramReport/View/Components/MaskingLabel.swift
@@ -0,0 +1,45 @@
+//
+//  MaskingLabel.swift
+//  AaramReport
+//
+//  Created by OBeris on 2/5/24.
+//
+//  https://stackoverflow.com/questions/39140306/masking-uiview-uiimageview-to-cutout-transparent-text
+
+import UIKit
+
+@IBDesignable
+class MaskingLabel: UILabel {
+    private var originalBackgroundColor: UIColor? = nil
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initLabel()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initLabel()
+    }
+
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: .zero))
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+
+        context.saveGState()
+        context.setBlendMode(.clear)
+
+        originalBackgroundColor?.setFill()
+        UIRectFill(rect)
+
+        super.drawText(in: rect)
+        context.restoreGState()
+    }
+
+    private func initLabel() {
+        // Label을 마스킹 하기 전에 background는 투명이어야 함. 그러므로 background를 미리 백업해둠.
+        originalBackgroundColor = backgroundColor
+        backgroundColor = .clear
+        self.clipsToBounds = true
+    }
+}
