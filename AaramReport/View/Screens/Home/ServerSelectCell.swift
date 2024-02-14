71fe50109cf576c9de8ea0673a030d2339b603f3
유환준 <eppal212@naver.com>
2024-02-14 16:32:58 +0900

    [기능] CollectionViewCell Highlight 추가

--- AaramReport/View/Screens/Home/ServerSelectCell.swift
+++ AaramReport/View/Screens/Home/ServerSelectCell.swift
@@ -3,4 +3,19 @@ import UIKit
 class ServerSelectCell: UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
+
+    // CollectionView는 SelectionStyle이 없어서 직접 구현
+    override var isHighlighted: Bool {
+        willSet {
+            if isHighlighted {
+                UIView.animate(withDuration: 0.1) { [weak self] in
+                    self?.alpha = 1.0
+                }
+            } else {
+                UIView.animate(withDuration: 0.1) { [weak self] in
+                    self?.alpha = 0.5
+                }
+            }
+        }
+    }
 }
